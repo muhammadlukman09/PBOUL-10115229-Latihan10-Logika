13608d093557d514a00f08bdd0babf7f2b6e15f3
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 10:15:43 +0700

    project kesepuluh

---
+++ src/pboul/pkg10115229/latihan10/logika/PBOUL10115229Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan10.logika;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
